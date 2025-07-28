155c621d47aa9bb03be99e41e3ac91ef3dc1ce08
DJCC <david.carcamo2006@gmail.com>
2025-07-28 15:33:50 -0600

    Desarrollo de clase abstracta y specifics de usuarios

---
+++ JAVA-TICKET/src/main/java/com/mycompany/java/ticket/userBase.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java.ticket;
+
+/**
+ *
+ * @author David
+ */
+public abstract class userBase {
+    private boolean derechos; //Esta variable es la que determinara si el usuario puede realizar ciertas acciones
+    private String nombre;
+    private String username;
+    private String password;
+    private int edad;
+    
+    //setters
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public void setUsername(String Username){
+        username=Username;
+    }
+    
+    public void setPassword(String password){
+        this.password=password;
+    }
+    
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+    
+    //getters
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+}
---
+++ JAVA-TICKET/src/main/java/com/mycompany/java/ticket/userContenidos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java.ticket;
+import java.util.ArrayList;
+/**
+ *
+ * @author David
+ */
+public class userContenidos extends userBase{
+    ArrayList<Integer> registroEventos = new ArrayList<Integer>();
+    
+}
